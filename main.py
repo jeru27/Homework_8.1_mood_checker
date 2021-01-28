350bf34f815d9223ea79aab6dcfec178c10f0b06
jeru27 <jrumora@gmail.com>
2021-01-28 15:33:37 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+
+mood = input("Choose your mood; happy, nervous, cheerful, relaxed, excited: ")
+
+
+if mood == "happy":
+    print("It is great to see you happy!")
+elif mood == "nervous":
+    print("take a deep breath 3 times.")
+elif mood == "cheerful":
+    print("Good on ya mate!")
+elif mood == "relaxed":
+    print("Love being relaxed!")
+elif mood == 'excited':
+    print("Exciting is good!")
+else:
+    print("I don't recognise this mood!")
+
